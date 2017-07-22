4d61046c619dab74f1a0d35ca12f7fc96cda6008
lys861205 <lys86_1205@126.com>
2017-07-22 11:38:22 +0800

    change

--- lru_test.go
+++ lru_test.go
@@ -5,27 +5,13 @@ import (
 	"fmt"
 )
 
-/* func TestSet(t *testing.T){ */
-	// cache := New(10)
-	// cache.Set("key", "liuyuansheng")
-	// cache.Set("key1", "gengxiaoqian")
-	// cache.Set("key2", "liuxiya")
-	// cache.Set("key3", "hubeisheng")
-	// cache.Set("key3", "hebei")
-	// v,err := cache.Get("key")
-	// if err != nil {
-	//     t.Error("Error get data", err)
-	// } else {
-	//     fmt.Println(v)
-	// }
-/* } */
 func TestSet1(t *testing.T){
 	cache := New(2)
-	cache.Set("key", "liuyuansheng")
-	cache.Set("key1", "gengxiaoqian")
-	cache.Set("key2", "liuxiya")
+	cache.Set("key", "sheng")
+	cache.Set("key1", "qian")
+	cache.Set("key2", "xiya")
 	cache.Set("key3", "hubeisheng")
-	cache.Set("key3", "hebei")
+	cache.Set("key3", "hebeisheng")
 	v,err := cache.Get("key3")
 	if err != nil {
 		t.Error("Error get data", err)
